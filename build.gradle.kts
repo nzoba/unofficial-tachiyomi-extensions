aa1f65a3b3ab72ef943da9eaa6cbfbc9b6145f44
nzoba <55888232+nzoba@users.noreply.github.com>
2024-02-03 13:42:42 +0100

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,6 @@ buildscript {
 }
 
 allprojects {
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_1_8.toString()
-        }
-    }
-
     repositories {
         mavenCentral()
         google()
